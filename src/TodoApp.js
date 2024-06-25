06d6ab57246521b41e11629a8f60ff00413ca38e
unknown <arpitapakki@gmail.com>
2024-06-25 13:01:57 +0530

    Initial commit

---
+++ src/TodoApp.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import './TodoApp.css';
+
+function TodoApp() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState('');
+
+  const addTodo = () => {
+    if (input.trim()) {
+      setTodos([...todos, { text: input, completed: false }]);
+      setInput('');
+    }
+  };
+
+  const toggleComplete = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].completed = !newTodos[index].completed;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = (index) => {
+    const newTodos = todos.filter((_, i) => i !== index);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="todo-app">
+      <h1>Todo List</h1>
+      <div className="todo-input">
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="Add a new task..."
+        />
+        <button onClick={addTodo}>
+          <i className="fas fa-plus"></i>
+        </button>
+      </div>
+      <ul className="todo-list">
+        {todos.map((todo, index) => (
+          <li key={index} className={todo.completed ? 'completed' : ''}>
+            <span onClick={() => toggleComplete(index)}>{todo.text}</span>
+            <button onClick={() => removeTodo(index)}>
+              <i className="fas fa-trash"></i>
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default TodoApp;
+
